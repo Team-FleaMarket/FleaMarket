80f9bc533323b448082ebdc834119551108b7758
Feooo <feoooyang@outlook.com>
2023-12-06 16:41:26 +0800

    change CreateTable sql file's variable name

--- sql/fleamarket/CreateTable.sql
+++ sql/fleamarket/CreateTable.sql
@@ -2,25 +2,25 @@ CREATE DATABASE fleamarket;
 USE fleamarket;
 CREATE TABLE users (
 	id INTEGER AUTO_INCREMENT,
-	studentNo VARCHAR(100),
+	student_no VARCHAR(100),
 	password VARCHAR(100),
 	email VARCHAR(100),
 	phone VARCHAR(100),
-	username VARCHAR(100),
-	weixin VARCHAR(100),
+	user_name VARCHAR(100),
+	wechat VARCHAR(100),
 	PRIMARY KEY(id)
 ) CHARACTER SET utf8mb4;
 
 CREATE TABLE goods (
 	id INTEGER AUTO_INCREMENT,
-	studentNo VARCHAR(100),
-	goodsName VARCHAR(100),
+	student_no VARCHAR(100),
+	goods_name VARCHAR(100),
 	description VARCHAR(100),
 	price DOUBLE,
 	status INTEGER,
 	cate VARCHAR(100),
-	imagepath VARCHAR(100),
-	goodsStatus INTEGER,
+	image_path VARCHAR(100),
+	goods_status INTEGER,
 	degree INTEGER,
 	PRIMARY KEY(id)
 ) CHARACTER SET utf8mb4;
