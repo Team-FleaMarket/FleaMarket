496683641e2b5b51b124281a5577803ba95fa71a
Viceam <m99viceam@outlook.com>
2023-12-12 08:58:16 +0800

    admin 表

--- sql/fleamarket/CreateTable.sql
+++ sql/fleamarket/CreateTable.sql
@@ -1,27 +1,35 @@
 CREATE DATABASE fleamarket;
 USE fleamarket;
-CREATE TABLE users (
-	id INTEGER AUTO_INCREMENT,
-	student_no VARCHAR(100),
-	password VARCHAR(100),
-	email VARCHAR(100),
-	phone VARCHAR(100),
-	user_name VARCHAR(100),
-	wechat VARCHAR(100),
-	PRIMARY KEY(id)
+CREATE TABLE users
+(
+    id         INTEGER AUTO_INCREMENT,
+    student_no VARCHAR(100),
+    password   VARCHAR(100),
+    email      VARCHAR(100),
+    phone      VARCHAR(100),
+    user_name  VARCHAR(100),
+    wechat     VARCHAR(100),
+    PRIMARY KEY (id)
 ) CHARACTER SET utf8mb4;
 
-CREATE TABLE goods (
-	id INTEGER AUTO_INCREMENT,
-	student_no VARCHAR(100),
-	goods_name VARCHAR(100),
-	description VARCHAR(100),
-	price DOUBLE,
-	status INTEGER,
-	cate VARCHAR(100),
-	image_path VARCHAR(100),
-	goods_status INTEGER,
-	degree INTEGER,
-	PRIMARY KEY(id)
+CREATE TABLE goods
+(
+    id           INTEGER AUTO_INCREMENT,
+    student_no   VARCHAR(100),
+    goods_name   VARCHAR(100),
+    description  VARCHAR(100),
+    price        DOUBLE,
+    status       INTEGER,
+    cate         VARCHAR(100),
+    image_path   VARCHAR(100),
+    goods_status INTEGER,
+    degree       INTEGER,
+    PRIMARY KEY (id)
 ) CHARACTER SET utf8mb4;
 
+create table admin
+(
+    id       INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100),
+    password VARCHAR(100)
+);
